831589f656d20b7ef99a0020a26c347f72ee80b6
Fraser Tweedale <frase@cpan.org>
2011-05-17 03:30:53 +0000

    Import of FRASE/Test-Builder-Clutch-0.04 from CPAN.
    gitpan-cpan-distribution: Test-Builder-Clutch
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         FRASE/Test-Builder-Clutch-0.04.tar.gz
    gitpan-cpan-author:       FRASE
    gitpan-cpan-maturity:     released

--- lib/Test/Builder/Clutch.pm
+++ lib/Test/Builder/Clutch.pm
@@ -9,11 +9,11 @@ Test::Builder::Clutch - add a clutch to your testing drivechain
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
